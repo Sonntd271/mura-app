688345a32c4fa49097d0cc8115c4187fa62928f7
Sonntd271 <itssonntd271@gmail.com>
2024-01-03 02:04:04 +0100

    Created a script for setting up kops instance and updated db creation queries

---
+++ scripts/kops.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+# ---------- Install Kubectl ---------- #
+# Ref: https://kubernetes.io/docs/tasks/tools/install-kubectl-linux/
+sudo apt-get update -y && sudo apt-get install ca-certificates curl -y
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update -y
+sudo apt-get install kubectl -y
+
+# ---------- Install kOps ---------- #
+# Ref: https://kops.sigs.k8s.io/getting_started/install/
+curl -Lo kops https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
+sudo chmod +x kops
+sudo mv kops /usr/local/bin/kops
+
+# ---------- Install Helm ---------- #
+# Ref: https://helm.sh/docs/intro/install/
+curl https://baltocdn.com/helm/signing.asc | sudo gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
+sudo apt-get install apt-transport-https -y # Test to see if can skip
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo apt-get update -y
+sudo apt-get install helm -y
